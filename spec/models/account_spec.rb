ae3b2a15e41400d1bbe46ad5121d3ca166d73c13
Michael Buchanan <micbuchanan@yahoo.com>
2014-10-12 12:40:04 +0100

    First rspec tests for user and account models

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Account, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @account = Account.new(name:         			"Example Account",
+  																primary_user: 			"Example Name",
+  																primary_user_email: "user@example.com",
+  																number_of_users:     1  ) }
+
+  subject { @account }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:primary_user) }
+  it { should respond_to(:primary_user_email) }
+  it { should respond_to(:number_of_users) }
 end
